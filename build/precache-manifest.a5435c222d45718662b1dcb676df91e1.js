3cda9dfce4aeae9afe23c9efd9b58e50b26b9f3c
khai <khaitruong1301@gmail.com>
2020-10-22 16:45:17 +0700

    'dp'

---
+++ build/precache-manifest.a5435c222d45718662b1dcb676df91e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8906abd81ff5da8766feb82772fb71bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078c6ff4637cc0f32973",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75793985320f2d84b642",
+    "url": "/static/js/2.cc2579de.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.cc2579de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078c6ff4637cc0f32973",
+    "url": "/static/js/main.9010d1cc.chunk.js"
+  },
+  {
+    "revision": "4ec4be10a0c259784fe3",
+    "url": "/static/js/runtime-main.ef976919.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
